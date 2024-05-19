ac26eb042d7a90655c6fbf0389726dd01c1a674e
Utkarsh Tiwari <uktiwari023@gmail.com>
2024-05-19 14:05:12 +0530

    complete all sections

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,12 +1,57 @@
-import React from 'react'
-import HeadText from './HeadText'
+import React from 'react';
+import HeadText from './HeadText';
 
 const Contact = () => {
   return (
-    <div>
+    <>
       <HeadText heading="Contact" />
-    </div>
-  )
-}
+      <form 
+        className='flex flex-col mt-10 gap-6 items-center bg-slate-700 p-4 md:w-2/3 mx-auto rounded-lg'
+      >
+        <div className='flex flex-col gap-2 items-start w-full'>
+          <label htmlFor="name" className='text-white text-lg'>
+            Name
+          </label>
+          <input 
+            type="text" 
+            required 
+            name='name' 
+            placeholder='Enter your name' 
+            className='border-none outline-none rounded-md p-2 text-lg md:w-full capitalize dark:text-white dark:bg-slate-900 bg-opacity-75'
+          />
+        </div>
+        <div className='flex flex-col gap-2 items-start w-full'>
+          <label htmlFor="email" className='text-white text-lg'>
+            Email
+          </label>
+          <input 
+            type="email" 
+            required 
+            name='email' 
+            placeholder='Enter your email' 
+            className='border-none outline-none rounded-md p-2 text-lg md:w-full capitalize dark:text-white dark:bg-slate-900 bg-opacity-75'
+          />
+        </div>
+        <div className='flex flex-col gap-2 items-start w-full'>
+          <label htmlFor="message" className='text-white text-lg'>
+            Message
+          </label>
+          <textarea 
+            name="message" 
+            rows="4" 
+            placeholder="Enter your message" 
+            className='border-none outline-none rounded-md p-2 dark:text-white text-lg md:w-full capitalize dark:bg-slate-900 bg-opacity-75 resize-none'
+          ></textarea>
+        </div>
+        <button 
+          type="submit" 
+          className='bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-md transition-all duration-300 ease-in-out'
+        >
+          Submit
+        </button>
+      </form>
+    </>
+  );
+};
 
-export default Contact
+export default Contact;
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -35,7 +35,13 @@ const ProjectCard = () => {
             {projects.map((item)=>{
                 return (
                     <div key={item.id} className='flex flex-col p-2 md:p-2 max-w-1/3 w-60 transition-all duration-200 ease-in-out dark:bg-gray-700 bg-gray-200 dark:text-white rounded-md hover:shadow-sm dark:hover:shadow-slate-100'>
-                        <img width={1000} src={item.imgSrc} className='w-60 h-32 object-cover' alt="projectName" />
+                        <img
+                            width={1000}
+                            height={562} 
+                            src={item.imgSrc}
+                            className='w-60 h-32 object-cover'
+                            alt="projectName"
+                        />
                         <h2 className='text-xl font-semibold'>{item.name}</h2>
                         <p className='dark:text-gray-300 text-xs'>{item.description}</p>
                         <div className='flex items-center justify-between w-full'>
