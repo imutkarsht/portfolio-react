f946fb9698492a058e6d6264c03b9ebe0cf52e6d
Utkarsh Tiwari <uktiwari023@gmail.com>
2024-08-07 19:27:21 +0530

    fix url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,4 +2,4 @@ export const scriptURL = 'https://script.google.com/macros/s/AKfycbw50fI3eEMVLWU
 
 export const LEET_URL = "https://leetcode-stats-api.herokuapp.com/imutkarsht";
 
-export const BACKEND_API = 'http://portfolio-backend-crdx.onrender.com/api/feedback'
+export const BACKEND_API = 'https://portfolio-backend-crdx.onrender.com/api/feedback'
